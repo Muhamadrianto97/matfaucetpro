9a6b14ef16bd4bee8ee9c44ec423ad63936288c4
muhamad rianto <muhtoevill>
2018-12-28 21:26:54 +0700

    MTC bot update v0.3

--- mat.php
+++ mat.php
@@ -7,15 +7,12 @@ function getStr($string,$start,$end){
 }
 function getmat($id,$socks){
 	$arr = array("\r","	");
-	$url = "http://saosdeveloper.club/mtc/intrewardv3.php";
-	$h = explode("\n",str_replace($arr,"","Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-	User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0
-	Accept-Language: id,en-US;q=0.7,en;q=0.3
-	Accept-Encoding: gzip, deflate
-	Host: saosdeveloper.club
+	$url = "http://saosdeveloper.club/mtc/irw/";
+	$h = explode("\n",str_replace($arr,"","Content-Type: application/json; charset=utf-8
 	Connection: Keep-Alive
-	Upgrade-Insecure-Requests: 1"));
-	$body = "iduser=$id&reward=a87ff679a2f3e71d9181a67b7542122c";
+	Accept-Encoding: gzip
+	User-Agent: okhttp/3.10.0"));
+	$body = "{\"iduser\": $id,\"reward\": \"45c48cce2e2d7fbdea1afc51c7c6ad2611\"}";
 	return curl($url,$h,$body,$socks);
 }
 function curl($url,$h,$body,$socks){
@@ -27,36 +24,30 @@ function curl($url,$h,$body,$socks){
 	curl_setopt($ch, CURLOPT_PROXY, $socks);
 	curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	$x = curl_exec($ch); 
+	$x = curl_exec($ch);
 	curl_close($ch);
 	return json_decode($x,true);
-	//var_dump($x);
-	//exit();
 }
 echo "#################\n#  @muhtoevill  #\n#   SGB-Team    #\n#################\n";
 echo "Socks File: ";
 $file = trim(fgets(STDIN));
 echo "Id Code	 :";
 $id = trim(fgets(STDIN));
-echo "Berapa Kali :";
-$loop = trim(fgets(STDIN));
-for ($i=1; $i <= $loop; $i++){
 $socks = explode("\n",str_replace("\r","",file_get_contents($file))); $a=0;
 while($a<count($socks)){
 	$proxy = $socks[$a];
+	sleep(120);
 	$submit = getmat($id,$proxy);
 	$output = json_encode($submit);
 	$notif = getStr($output,'"notif":',',');
 	$wallet = getStr($output,'"wallet":','}');
 	if(strpos($output,"wallet")==true){
-                $text = "Use Proxy $proxy Berhasil $notif  Wallet Anda Sekarang : $wallet Credit By:Muhtoevill";
+                $text = "Use Proxy $proxy Berhasil $notif  Wallet Anda Sekarang : $wallet Delay 2 menit Credit By:Muhtoevill";
                 $text1 = "\033[32m".$text."\033[0m";
             }else{
                 $text ="Use Proxy $proxy Socks Die";
                 $text1 = "\033[31m".$text."\033[0m";
 				$a++;
-                
-        }
+				}
 	echo $text1."\n";
-}
 }
